df69438e6648162e77039321cdaeb920e10e29ad
moh295 <mohammed@clearimageai.com>
2022-06-07 13:57:47 +0200

    for cuda

--- app.py
+++ app.py
@@ -11,14 +11,16 @@ PATH = '/App/data/cifar_net.pth'
 
 
 if __name__ == '__main__':
- #check_data_and_lable()
+ check_data_and_lable()
 
  # stat_dic=start_training(net,5,trainloader,optimizer,criterion)
  # print('saving checkpoint to ',PATH)
  # torch.save(stat_dic, PATH)
 
- # random_check(net,PATH)
+ #random_check(net,PATH)
  # overall_check(net,PATH)
  # each_class_check(net,PATH)
  x = torch.ones((1, 3, 224, 224)).cuda()
- start_converting(net,x)
+
+ print(trainloader[0].shap)
+ start_converting(net,trainloader[0])
