22415d13c77311b5df73959011fb8cb129437662
moh295 <mohammed@clearimageai.com>
2022-06-07 14:11:40 +0200

    for cuda

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from validation import  random_check , overall_check ,each_class_check
 from convert import start_converting
 
 PATH = '/App/data/cifar_net.pth'
-
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 if __name__ == '__main__':
  #check_data_and_lable()
@@ -23,5 +23,5 @@ if __name__ == '__main__':
  # x = torch.ones((1, 3, 5, 5)).cuda()
  dataiter = iter(trainloader)
  # print(dataiter.next())
- x=dataiter.next()
+ x=dataiter.next().to(device)
  start_converting(net,x)
