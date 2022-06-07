9da8f8294d21c9bef6cf4c3da39789276fb1f21a
moh295 <mohammed@clearimageai.com>
2022-06-07 17:30:30 +0200

    train trt

--- validation.py
+++ validation.py
@@ -36,8 +36,9 @@ def overall_check(model,checkpoint):
     from collections import OrderedDict
     new_state_dict = OrderedDict()
     for k, v in state_dict.items():
-        name = k[7:]  # remove `module.`
         print(k)
+        name = k[7:]  # remove `module.`
+
         new_state_dict[name] = v
     # load params
     model.load_state_dict(new_state_dict)
