81a1161870be2210c9d459ebdea695bfb2deda99
moh295 <mohammed@clearimageai.com>
2022-06-07 10:38:27 +0200

    simple model

---
+++ myModel.py
@@ -0,0 +1,49 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 4
+download_data=False
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=download_data, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=download_data, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+if __name__ == '__main__':
+
+
+    # functions to show an image
+    def imshow(img):
+        img = img / 2 + 0.5     # unnormalize
+        npimg = img.numpy()
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+        plt.show()
+
+
+    # get some random training images
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+
+    # show images
+    imshow(torchvision.utils.make_grid(images))
+    # print labels
+    print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
