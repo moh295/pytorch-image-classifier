5350216cfc123bfdb980bef8f9b4f91879c9a145
moh295 <mohammed@clearimageai.com>
2022-06-07 16:56:16 +0200

    train trt

--- convert.py
+++ convert.py
@@ -6,13 +6,13 @@ TRT_TRAINED='/App/data/trained_trt.pth'
 
 def start_converting(model,input,batch_size):
     print('start converting...')
-    model_trt = torch2trt(model, [input])
+    model_trt = torch2trt(model, [input],max_batch_size=batch_size)
     y = model(input)
     y_trt = model_trt(input)
 
     # check the output against PyTorch
     print('diff',torch.max(torch.abs(y - y_trt)))
     print('save trt')
-    torch.save(model_trt.state_dict(), TRT_TRAINED,max_batch_size=batch_size)
+    torch.save(model_trt.state_dict(), TRT_TRAINED)
 
     return model_trt
--- dataloader.py
+++ dataloader.py
@@ -7,7 +7,7 @@ transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-batch_size = 1
+batch_size = 4
 download_data=True
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
