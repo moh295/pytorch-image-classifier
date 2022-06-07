0b807aa6037c5e23341d42d96e434bb1ad283417
moh295 <mohammed@clearimageai.com>
2022-06-07 11:48:19 +0200

    simple model

--- app.py
+++ app.py
@@ -5,12 +5,22 @@ from dataloader import trainloader
 from train import start_training
 from model import net,optimizer,criterion
 from validation import  random_check , overall_check ,each_class_check
+from timeit import default_timer as timer
+from datetime import timedelta
+
+
+
+
 if __name__ == '__main__':
  #check_data_and_lable()
- stat_dic=start_training(net,2,trainloader,optimizer,criterion)
+ start = timer()
+ stat_dic=start_training(net,5,trainloader,optimizer,criterion)
+ end = timer()
+ print('traning time',timedelta(seconds=end - start))
  PATH = './cifar_net.pth'
- # print('saving checkpoint to ',PATH)
- # torch.save(stat_dic, PATH)
+
+ print('saving checkpoint to ',PATH)
+ torch.save(stat_dic, PATH)
  random_check(net,PATH)
  overall_check(net,PATH)
  each_class_check(net,PATH)
--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 import torch
+from timeit import default_timer as timer
+from datetime import timedelta
 
 def start_training(model,epochs,trainloader,optimizer,criterion):
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -6,6 +8,7 @@ def start_training(model,epochs,trainloader,optimizer,criterion):
     # Assuming that we are on a CUDA machine, this should print a CUDA device:
 
     print(device)
+    start = timer()
     for epoch in range(epochs):  # loop over the dataset multiple times
 
         running_loss = 0.0
@@ -25,7 +28,9 @@ def start_training(model,epochs,trainloader,optimizer,criterion):
             # print statistics
             running_loss += loss.item()
             if i % 2000 == 1999:    # print every 2000 mini-batches
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                end = timer()
+                elapsed=timedelta(seconds=end - start)
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f} elapsed {elapsed}')
                 running_loss = 0.0
 
     print('Finished Training')
