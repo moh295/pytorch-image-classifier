06742cc937cb36dd84a2341ca20bba77587f8d08
moh295 <mohammed@clearimageai.com>
2022-06-07 17:24:08 +0200

    train trt

--- convert.py
+++ convert.py
@@ -14,6 +14,6 @@ def start_converting(model,input,batch_size):
     # check the output against PyTorch
     print('diff',torch.max(torch.abs(y - y_trt)))
     print('save trt',TRT_TRAINED)
-    torch.save(model_trt.state_dict(), TRT_TRAINED)
+    torch.save(model_trt.module.state_dict(), TRT_TRAINED)
 
     return model_trt
