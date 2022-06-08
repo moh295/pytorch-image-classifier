3ebec2b911acf272b88609b29a2adfbe5b4217a8
moh295 <mohammed@clearimageai.com>
2022-06-08 21:03:34 +0200

    add images

--- build_dataset.py
+++ build_dataset.py
@@ -12,14 +12,14 @@ print (df[id].iloc[0])
 
 data_lenght=len(df[id])
 for i in range(data_lenght):
-    if  df[label].iloc[i]!=0:
+    if  df[label].iloc[i]==0:
         file_name=str(df[id].iloc[i])+image_fomrat
 
         #Path(folders_path+'dog/'+file_name).rename(folders_path+'test/'+file_name)
         from_path=folders_path+'dog/'+file_name
-        to_path=folders_path+'cat/'+file_name
+        to_path=folders_path+'temp/'+file_name
         try:
-            print('fil', file_name ,'moved to',to_path)
+            print('file', file_name ,'moved to',to_path)
             shutil.move(from_path ,to_path)
 
         except:pass
