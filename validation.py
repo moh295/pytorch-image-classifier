dc328157039c3c161d8b1c07a2fb6c256bd1d9d0
moh295 <mohammed@clearimageai.com>
2022-06-08 18:21:49 +0200

    add images

--- validation.py
+++ validation.py
@@ -127,3 +127,28 @@ def torch2trt_check(model,model_trt,loder):
     elapsed = timedelta(seconds=end - start)
     print(f'TensorRT predction of {numpred} takes {elapsed}')
     print(f'TensorRT Accuracy of the network on the 10000 test images: {100 * correct // total} %')
+
+def overall_check2(model,loder):
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    train_loader, val_loader = loder
+    model.eval()
+    correct = 0
+    total = 0
+    # since we're not training, we don't need to calculate the gradients for our outputs
+    with torch.no_grad():
+        numpred=0
+        start=timer()
+        for data in val_loader:
+            numpred+=1
+            images, labels = data[0].to(device), data[1].to(device)
+            # calculate outputs by running images through the network
+            outputs = model(images)
+            # the class with the highest energy is what we choose as prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    end = timer()
+    elapsed = timedelta(seconds=end - start)
+    print(f'predction of {numpred} takes {elapsed}')
+    print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
