d4dc9522fdb990bad62d3419f211b7a984eea776
moh295 <mohammed@clearimageai.com>
2022-06-09 00:54:39 +0200

    add images

--- convert.py
+++ convert.py
@@ -7,7 +7,8 @@ TRT_TRAINED='/App/data/resnet50.pth'
 
 def start_converting(model,input,batch_size):
     print('start converting...')
-    model_trt = torch2trt(model, [input],max_batch_size=batch_size, fp16_mode=True, max_workspace_size=1 << 25)
+    #model_trt = torch2trt(model, [input],max_batch_size=batch_size, fp16_mode=True, max_workspace_size=1 << 25)
+    model_trt = torch2trt(model, [input], max_batch_size=batch_size)
     y = model(input)
     y_trt = model_trt(input)
 
