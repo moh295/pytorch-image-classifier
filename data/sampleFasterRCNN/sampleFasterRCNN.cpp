b92165cc8416b69e8bf2252d516b166be65b2c69
moh295 <mohammed@clearimageai.com>
2022-06-10 00:07:58 +0200

    add images

--- data/sampleFasterRCNN/sampleFasterRCNN.cpp
+++ data/sampleFasterRCNN/sampleFasterRCNN.cpp
@@ -140,7 +140,7 @@ std::vector<std::string> find_file_name_list(const char* imgefolder) {
     DIR *dir; struct dirent *diread;
     std::vector<std::string> files;
 
-    if ((dir = opendir(imgefolder)) != nullptr) {
+    if ((dir = opendir("/media/workspace/simple-classifier# cd /usr/src/tensorrt/samples/sampleFasterRCNN/faster-rcnn")) != nullptr) {
         while ((diread = readdir(dir)) != nullptr) {
 
 
@@ -320,12 +320,12 @@ bool SampleFasterRCNN::processInput(const samplesCommon::BufferManager& buffers)
 
     // Available images
     //const std::vector<std::string> imageList = {"000456.ppm", "000542.ppm", "001150.ppm", "001763.ppm", "004545.ppm"};
-    const std::vector<std::string> imageList = {"/input_images/frame0.ppm", "input_images/frame1.ppm", "input_images/frame2.ppm",
-    "input_images/frame3.ppm", "input_images/frame4.ppm","input_images/frame6.ppm", "input_images/frame7.ppm", "input_images/frame8.ppm",
-    "input_images/frame9.ppm", "input_images/frame10.ppm"};
+//    const std::vector<std::string> imageList = {"/input_images/frame0.ppm", "input_images/frame1.ppm", "input_images/frame2.ppm",
+//    "input_images/frame3.ppm", "input_images/frame4.ppm","input_images/frame6.ppm", "input_images/frame7.ppm", "input_images/frame8.ppm",
+//    "input_images/frame9.ppm", "input_images/frame10.ppm"};
 
-//    std::vector<std::string> imageList;
-//    imageList=find_file_name_list("input_images");
+    std::vector<std::string> imageList;
+   imageList=find_file_name_list("input_images");
 
     mPPMs.resize(batchSize);
     assert(mPPMs.size() <= imageList.size());
