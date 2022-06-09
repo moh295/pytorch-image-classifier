5e85f9358819a55349fcc45e67e1c2f54cd9e4e8
moh295 <mohammed@clearimageai.com>
2022-06-09 17:20:23 +0200

    c++ search for ppm

---
+++ printfiles.cpp
@@ -0,0 +1,27 @@
+//
+// Created by mom44 on 09/06/2022.
+//
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+using std::cout; using std::cin;
+using std::endl; using std::string;
+using std::filesystem::directory_iterator;
+using std::filesystem::directory_iterator;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+
+
+    string path = "C:/Users/mom44/Desktop/temp ppm images";
+
+    for (const auto & file : directory_iterator(path))
+        cout << file.path() << endl;
+
+    return EXIT_SUCCESS;
+
+    return 0;
+}
+
