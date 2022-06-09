699cfd473a96ddcdc2a460e47cf632a47f65a2e5
moh295 <mohammed@clearimageai.com>
2022-06-09 23:12:14 +0200

    add images

--- data/sampleFasterRCNN/sampleFasterRCNN.cpp
+++ data/sampleFasterRCNN/sampleFasterRCNN.cpp
@@ -136,7 +136,7 @@ private:
  std::vector<std::string>  find_file_name_list(DIR);
 //more shit..
 
-std::vector<std::string> find_file_name_list(DIR *imgefolder) {
+std::vector<std::string> find_file_name_list(char imgefolder) {
     DIR *dir; struct dirent *diread;
     std::vector<std::string> files;
 
