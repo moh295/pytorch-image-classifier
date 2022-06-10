4d806c9d54649960b6b3635f85b98dbc2eefcb79
moh295 <mohammed@clearimageai.com>
2022-06-10 17:24:18 +0200

    _

--- export_images.py
+++ export_images.py
@@ -11,17 +11,25 @@ def extractImages(pathIn, pathOut):
         #vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))    # added this line
         success,image = vidcap.read()
         print ('Read a new frame: ', success)
+
         try:
-            cv2.imwrite( pathOut + "\\frame%d.ppm" % count, image)     # save frame as JPEG file
+            resized = resize(image, 500, 375)
+            cv2.imwrite( pathOut + "\\frame%d.ppm" % count, resized)     # save frame as JPEG file
             count = count + 1
         except:pass
 
+def resize(src,w,h):
+    width = int(w)
+    height = int(h)
+    dim = (width, height)
 
+    # resize image
+    return  cv2.resize(src, dim, interpolation=cv2.INTER_AREA)
 
 if __name__=="__main__":
 
     video_path='C:/Users/mom44/Google Drive (mohammed@clearimageai.com)/projects/4-loss-prevention/media/raw_videos/input.mp4'
-    image_folder='data/ppm_folder'
+    image_folder='data/sampleFasterRCNN/faster-rcnn'
     a = argparse.ArgumentParser()
     a.add_argument("--pathIn", help="path to video",default=video_path)
     a.add_argument("--pathOut", help="path to images",default=image_folder)
