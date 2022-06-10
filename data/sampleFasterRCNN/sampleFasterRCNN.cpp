f519e23fdba2ec432e29751a226e8bf501b1b70d
moh295 <mohammed@clearimageai.com>
2022-06-10 11:06:06 +0200

    add images

--- data/sampleFasterRCNN/sampleFasterRCNN.cpp
+++ data/sampleFasterRCNN/sampleFasterRCNN.cpp
@@ -247,7 +247,7 @@ void SampleFasterRCNN::constructNetwork(SampleUniquePtr<nvcaffeparser1::ICaffePa
     }
 
     builder->setMaxBatchSize(mParams.batchSize);
-    config->setMaxWorkspaceSize(16_MiB);
+    config->setMaxWorkspaceSize(64_MiB);
     samplesCommon::enableDLA(builder.get(), config.get(), mParams.dlaCore);
 }
 
