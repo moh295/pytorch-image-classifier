734c559a9cf90e83d90b0e5cc1aef43780a94728
moh295 <mohammed@clearimageai.com>
2022-06-13 15:54:31 +0200

    _

--- convert.py
+++ convert.py
@@ -8,7 +8,7 @@ TRT_TRAINED='/App/data/resnet50.pth'
 def start_converting(model,input,batch_size):
     print('start converting...')
     #model_trt = torch2trt(model, [input],max_batch_size=batch_size, fp16_mode=True, max_workspace_size=1 << 25)
-    model_trt = torch2trt(model, [input], max_batch_size=batch_size)
+    model_trt = torch2trt(model, [input], max_batch_size=batch_size,fp16_mode=True)
     y = model(input)
     y_trt = model_trt(input)
 
---
+++ convert_wit_onnx.py
@@ -0,0 +1,29 @@
+# Some standard imports
+import io
+import numpy as np
+
+from torch import nn
+import torch.utils.model_zoo as model_zoo
+import torch.onnx
+
+PATH='/App/data/new_trt.pth'
+TRT_TRAINED='/App/data/resnet50.pth'
+
+def onnx_start_converting(model,input,batch_size):
+    print('start converting...')
+    input_names='input'
+    output_names= 'output'
+    model_onnx=torch.onnx.export(model,
+                                 input,
+                                 "resnet50.onnx",
+                                 verbose=False,
+                                 input_names=input_names,
+                                 output_names=output_names,
+                                 export_params=True,
+                                 )
+
+    print('model is converted ')
+
+
+
+    return model_onnx
