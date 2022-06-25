fefa81977f04196d514fa1b40820e2ab5913f793
moh295 <mohammed@clearimageai.com>
2022-06-26 00:50:57 +0200

    rcnn torchvison tools

--- app_fasterrcnn_torchvision.py
+++ app_fasterrcnn_torchvision.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     #loading/checking data....
 
-    batch_size=1
+    batch_size=62
     input_size=320
 
     train_loader, val_loader,classes =load_data(img_dir,input_size,batch_size,'train',True,0.7)
