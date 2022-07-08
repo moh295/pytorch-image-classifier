3e64ab4630813628a833b9a01014e13d329896dd
moh295 <mohammed@clearimageai.com>
2022-07-08 13:23:13 +0200

    inference

--- app_fasterrcnn_mobilenet_torchvision.py
+++ app_fasterrcnn_mobilenet_torchvision.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     #loading model
 
     model = models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=False)
-    model = model.load_state_dict(model).to(device)
+    model.load_state_dict(torch.load(TORCH_TRAINED)).to(device)
     model.eval()
 
 
